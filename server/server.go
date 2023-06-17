2e466dcbd7a251e5ef7c8ec4fb547c9859db1d20
qiruizheng <4271439+qiruizheng@users.noreply.github.com>
2023-06-17 21:06:10 +0800

    Update server.go

--- server/server.go
+++ server/server.go
@@ -16,8 +16,8 @@ import (
 	"time"
 
 	"github.com/NYTimes/gziphandler"
-	"github.com/qiruizheng/cloud-torrent/engine"
-	"github.com/qiruizheng/cloud-torrent/static"
+	"github.com/jpillora/cloud-torrent/engine"
+	"github.com/jpillora/cloud-torrent/static"
 	"github.com/jpillora/cookieauth"
 	"github.com/jpillora/requestlog"
 	"github.com/jpillora/scraper/scraper"
